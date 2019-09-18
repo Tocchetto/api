90c989131eb1b2902ddf935c3fb8f194e27c33c5
Guilherme <tocchettoo@gmail.com>
2019-09-18 03:41:05 -0300

    Arquivo que cria conexão e carrega os models criado

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Worlds' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Guilherme Tocchetto',
+    email: 'tocchettoo@gmail.com',
+    password_hash: 'senha_dificil',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
